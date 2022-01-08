04d3ba59fdecb01b98ddf18c1f7a4023a60521ba
Diego Dev <diegodev@Diegos-MacBook-Air.local>
2022-01-07 21:43:57 -0500

    commit before new login-feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
   {
-    path: '',
+    path: 'game',
     loadChildren: () =>
       import('./features/game-board/game-board.module').then(
         (m) => m.GameBoardModule
       ),
   },
-  { path: 'word-list', loadChildren: () => import('./word-list/word-list.module').then(m => m.WordListModule) },
-
-
 ];
 
 @NgModule({
